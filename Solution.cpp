1397a64b512e4afd9faea374b34c679a5933669c
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-19 11:08:03 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,25 @@
+
+/**
+ * @param {string} input
+ * @return {number}
+ */
+var scoreOfParentheses = function (input) {
+
+    const size = input.length;
+    let balance = 0;
+    let totalScore = 0;
+
+    for (let i = 0; i < size; i++) {
+        if (input.charAt(i) === '(') {
+            balance++;
+            continue;
+        }
+        balance--;
+
+        // (i-1)>=0 is not necessary becasue all parentheses are balanced.
+        if (input.charAt(i - 1) === '(') {
+            totalScore += (1 << balance);
+        }
+    }
+    return totalScore;
+};

cf9b7fda75517a422b70bbacdaeb65bac5292c5c
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-19 11:07:34 +0100

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+
+    public int scoreOfParentheses(String input) {
+
+        final int size = input.length();
+        int balance = 0;
+        int totalScore = 0;
+        
+        for (int i = 0; i < size; i++) {
+            if (input.charAt(i) == '(') {
+                balance++;
+                continue;
+            }
+            balance--;
+            
+            // (i-1)>=0 is not necessary becasue all parentheses are balanced.
+            if (input.charAt(i - 1) == '(') {
+                totalScore += (1 << balance);
+            }
+        }
+        return totalScore;
+    }
+}
